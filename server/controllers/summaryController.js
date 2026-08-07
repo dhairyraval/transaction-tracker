db56723c7cd5d3d29fd54f71115c3faf674625ea
Nuey2310 <67278622+dhairyraval@users.noreply.github.com>
2026-08-07 18:23:03 +0530

    feat: add API /summary (aggregation pipelines)

--- server/controllers/summaryController.js
+++ server/controllers/summaryController.js
@@ -1,38 +1,72 @@
 import Transaction from "../models/Transaction.js";
 
+// --- HELPER FUNCTION (Aggregation Pipes) ---
+
+const getTotals = () => 
+  Transaction.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalIn: { $sum: { $cond: [{ $eq: ["$type", "CREDIT"] }, "$amount", 0] } },
+        totalOut: { $sum: { $cond: [{ $eq: ["$type", "DEBIT"] }, "$amount", 0] } }
+      }
+    }
+  ]);
+
+const getCategoryTotals = () =>
+  Transaction.aggregate([
+    {
+      $group : { 
+        _id : "$category",
+        totalAmounts: { $sum: "$amount"}
+      } 
+    }
+  ]);
+
+const getMonthlyTotals = () =>
+  Transaction.aggregate([
+    {
+      $group : { 
+        _id: { year: { $year: "$date" }, month: { $month: "$date" } },
+        totalIn: { $sum: { $cond: [{ $eq: ["$type", "CREDIT"] }, "$amount", 0] } },
+        totalOut: { $sum: { $cond: [{ $eq: ["$type", "DEBIT"] }, "$amount", 0] } }
+      }
+    }
+  ]);
+
+const getLargestExpenses = () => 
+  Transaction.aggregate([
+    { $match: { type: "DEBIT" } },
+    { $sort: { amount: -1 } },
+    { $limit: 5 }
+  ]);
+
 
 export async function getSummary(req, res){
     
     try{
-        
-      const result = await Transaction.aggregate([
-      //1. Match for CREDIT
-      {
-        $match: { type: 'CREDIT' }
-      },
 
-      //2. Get sum of filtered items
-      {
-        $group: {
-          _id: null,
-          sumAmt: {$sum: '$amount'}
-        }
+      const [totals, categoriesTotals, monthlyTotals, expenses] = await Promise.all([
+        getTotals(),
+        getCategoryTotals(),
+        getMonthlyTotals(),
+        getLargestExpenses()
+      ]);
+
+      let netDiff = 0;
+      if (totals){
+        netDiff = totals[0].totalIn - totals[0].totalOut;
       }
-    ]);
-
-    const totalEarnings = result.length > 0 ? result[0].sumAmt : 0;
-    res.status(200).json({ totalEarnings });
+    
+      res.status(200).json({
+        totals: totals[0] || { totalIn: 0, totalOut: 0 },
+        netDiff,
+        categoriesTotals,
+        monthlyTotals,
+        expenses
+      });
 
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-
-  // total money in
-  const totalEarnings = async () => {
-    
-    return res.sumAmt;
-  };
-
-  res.status(200).json(totalEarnings);
-
 }
