ccaa4f77e519a78b403f297b9facf8187834801d
Nuey2310 <67278622+dhairyraval@users.noreply.github.com>
2026-08-12 11:46:51 +0530

    feat: deploy

--- server/server.js
+++ server/server.js
@@ -1,29 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 import transactionsRouter from "./routes/transactionRoutes.js";
 import summaryRouter from "./routes/summaryRoutes.js";
 import categoryRouter from "./routes/categoryRoutes.js"
 import { connectDB } from "./config/db.js";
-// const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
-// // Middleware
-app.use(cors());
+// Middleware
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 app.use(express.json());
 
 app.use("/api/transactions", transactionsRouter)
 app.use("/api/summary", summaryRouter)
 app.use("/api/categories", categoryRouter)
-const PORT = process.env.PORT || 5001;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")))
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
+  })
+}
 
 connectDB().then(() => {
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 
 })
